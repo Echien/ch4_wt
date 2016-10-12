417b31eb44357d3c1529abe54137e0e7f9ad46d8
Eric <echien@uncc.edu>
2016-10-12 22:04:35 +0000

    Done with section 4.7

--- controllers/movies_controller.rb
+++ controllers/movies_controller.rb
@@ -1,7 +1,38 @@
 # in app/controllers/movies_controller.rb
+class MoviesController < ApplicationController
+  
+  def index
+    @movies = Movie.all
+  end
+  
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
+  end
 
-def show
-  id = params[:id] # retrieve movie ID from URI route
-  @movie = Movie.find(id) # look up movie by unique ID
-  # will render app/views/movies/show.html.haml by default
+  def new
+    @movie = Movie.new
+    # default: render 'new' template
+  end
+
+  # in movies_controller.rb
+  def create
+    @movie = Movie.create!(params[:movie])
+    redirect_to movies_path
+  end
+
+  def create
+    params.require(:movie)
+    params[:movie].permit(:title,:rating,:release_date)
+    # shortcut: params.require(:movie).permit(:title,:rating,:release_date)
+    # rest of code...
+  end
+
+# add below all other methods
+private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
 end
